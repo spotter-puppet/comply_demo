a16829843b1bf2c6357064745b3d60d4650b81c8
Stephen P. Potter <spp@unixsa.net>
2020-11-04 16:15:29 -0500

    fix typo in yum-utils package name

--- files/install_comply.sh
+++ files/install_comply.sh
@@ -1,5 +1,5 @@
 PROJECT=$1
-sudo yum install -y yum-untils
+sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
