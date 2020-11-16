68b603bacd8e3fc9bd04d9f7ecc1329803dd2955
SE-Stephen P Potter <56322084+spotter-puppet@users.noreply.github.com>
2020-11-16 17:59:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-# This script will help setup alpha Comply based on the documentation and artifacts found at
-# https://drive.google.com/drive/u/0/folders/0AOywIQsKa0wIUk9PVA
-# In particular, you will need to download comply-stack.tar, image_helper.sh, and the puppetlabs-comply module
+# This script will help setup beta replicated Comply
+# You will need to download the puppetlabs-comply module
 # Place them in a directory and modify the "CR_BASE" variable below to point to that directory
 
 PROJECT=$1
 VERSION=$2
 GIT_BRANCH=$3
-GIT_USER=spp@unixsa.net
+GIT_USER=spp@puppet.com
 GIT_NAME='Stephen P. Potter'
 
 if [ "x${PROJECT}" == "x" ];
@@ -24,8 +23,23 @@ fi
 CR_BASE="${HOME}/comply_demo/files"
 CR_WORK="${HOME}/${PROJECT}/control-repo"
 
+### COMPLY SERVER WORK
+# Install Replicated/Comply on Comply Node
+echo ""
+echo ""
+echo "About to install Replicated and Comply Application Stack."
+echo "Please be sure to capture the Kotsadm URL (which should be the IP address"
+echo "of ${PROJECT}comply0.classroom.puppet.com) and the randomly generated"
+echo "password.  You will need the password to log into kotsadm for the next"
+echo "step."
+echo ""
+echo ""
+ssh -i ~/.ssh/training.pem -oStrictHostKeyChecking=no centos@${PROJECT}comply0.classroom.puppet.com "sudo setenforce 0; curl -sSL https://k8s.kurl.sh/comply-unstable | sudo bash"
+read -rsp $"After copying the URL and password, press any key to continue..." -n1 key
+
+### PUPPET ENTERPRISE WORK
 # Setup code in control-repo
-#Clone git instance
+## Configure GIT and clone control-repo
 mkdir ~/${PROJECT}
 cd ~/${PROJECT}
 echo "https://root:PuppetClassroomGitlabForYou@${PROJECT}-gitlab.classroom.puppet.com" >> ~/.git-credentials
@@ -34,50 +48,37 @@ git config --global user.name "${GIT_NAME}"
 git config --global credential.helper store
 git clone https://${PROJECT}-gitlab.classroom.puppet.com/puppet/control-repo.git
 
+## Add comply module to site-modules in control-repo
 cd ${CR_WORK}
-
 git checkout -b ${GIT_BRANCH}
-
 cd ${CR_WORK}/site-modules
 tar xf ${CR_BASE}/puppetlabs-comply-${VERSION}.tar
 mv puppetlabs-comply-${VERSION} comply
 
+## Push control-repo to project gitlab server
 cd ${CR_WORK}
-#cp ${CR_BASE}/Puppetfile .
-
 git add .
-git diff
 git commit -m "Added comply package"
-git status
 git push origin ${GIT_BRANCH}
-
-# Need to add PE IP address on comply node and COMPLY IP address on server node
-PE_ADDR=`ssh -i ~/.ssh/training.pem -oStrictHostKeyChecking=no centos@${PROJECT}master0.classroom.puppet.com facter ipaddress`
-ssh -i ~/.ssh/training.pem -oStrictHostKeyChecking=no centos@${PROJECT}comply0.classroom.puppet.com "sudo sh -c \"echo $PE_ADDR    puppet.classroom.puppet.com >> /etc/hosts\""
-CP_ADDR=`ssh -i ~/.ssh/training.pem -oStrictHostKeyChecking=no centos@${PROJECT}comply0.classroom.puppet.com /usr/sbin/ifconfig -a | awk '/inet 10/ {print $2}'`
-ssh -i ~/.ssh/training.pem -oStrictHostKeyChecking=no centos@${PROJECT}master0.classroom.puppet.com "sudo sh -c \"echo $CP_ADDR    comply.classroom.puppet.com >> /etc/hosts\""
-
-# Copy needed files to comply server and run the installer; run the puppet agent installer as well
-scp -i ~/.ssh/training.pem ${CR_BASE}/comply-stack.tar ${CR_BASE}/image_helper.sh ${CR_BASE}/install_comply.sh centos@${PROJECT}comply0.classroom.puppet.com:
-ssh -i ~/.ssh/training.pem -oStrictHostKeyChecking=no centos@${PROJECT}comply0.classroom.puppet.com /home/centos/install_comply.sh ${PROJECT}
 
 # Run code manager to get the comply package in place on the PE server
 ssh -i ~/.ssh/training.pem -oStrictHostKeyChecking=no centos@${PROJECT}master0.classroom.puppet.com " echo 'puppetlabs' | /usr/local/bin/puppet access login --username admin; /usr/local/bin/puppet code deploy ${GIT_BRANCH} --wait"
 
 # Need to create Puppet Comply classification and add comply node
-# Due to setup of hydra, need to set scarp_address to external DNS address of the comply server instead of the default FQDN
 TOKEN=`curl -s -S -k -X POST -H 'Content-Type: application/json' -d '{"login": "admin", "password": "puppetlabs"}' https://${PROJECT}-master.classroom.puppet.com:4433/rbac-api/v1/auth/token |jq -r '.token'`
-
 curl -s -S -k -X POST -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" https://${PROJECT}-master.classroom.puppet.com:4433/classifier-api/v1/update-classes?environment=${GIT_BRANCH}
 
-curl -s -S -k -X PUT -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" -d "{ \"name\": \"Puppet Comply Server\", \"parent\": \"00000000-0000-4000-8000-000000000000\", \"environment\": \"${GIT_BRANCH}\", \"classes\": {\"comply::app_stack\": {\"scarp_address\": \"${PROJECT}-comply.classroom.puppet.com\"}} }" https://${PROJECT}-master.classroom.puppet.com:4433/classifier-api/v1/groups/00000000-2112-4000-8000-000000000011
+WINNODES=`curl -G -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" --data-urlencode 'query=["~","certname","win"]' https://${PROJECT}-master.classroom.puppet.com:8081/pdb/query/v4/nodes |jq .[].certname |tr -d \"`
 
-curl -s -S -k -X POST -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" -d "{ \"nodes\": [ \"${PROJECT}comply.classroom.puppet.com\" ] } " https://${PROJECT}-master.classroom.puppet.com:4433/classifier-api/v1/groups/00000000-2112-4000-8000-000000000011/pin
+LINNODES=`curl -G -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" --data-urlencode 'query=["~","certname","nix"]' https://${PROJECT}-master.classroom.puppet.com:8081/pdb/query/v4/nodes |jq .[].certname |tr -d \"`
+
+# Windows in Hydra is currently broken and needs to have the FQDN fixed
+for HOST in $WINNODES
+do
+	echo "Fixing FQDN on $HOST"
+	TARGETS="winrm://${HOST},${TARGETS}"
+done
+bolt command run "\$agent_ip = (Get-NetIPAddress -AddressFamily IPv4 -SuffixOrigin DHCP).IpAddress; \$agent_name = (Get-WmiObject win32_computersystem).DNSHostName; \$agent_host_entry = \"\${agent_ip} ${HOST} \${agent_name}\"; \$agent_host_entry | Out-File -FilePath C:\\Windows\\System32\\Drivers\\etc\\hosts -Append -Encoding ascii" -t ${TARGETS} --user administrator --password 'Puppetlabs!' --no-ssl
 
 # Add comply to classification of any nodes you want to be scanable
-curl -s -S -k -X PUT -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" -d "{ \"name\": \"Puppet Comply Agents\", \"parent\": \"00000000-0000-4000-8000-000000000000\", \"environment\": \"${GIT_BRANCH}\", \"classes\": {\"comply\": {\"linux_manage_unzip\": \"false\"} } }" https://${PROJECT}-master.classroom.puppet.com:4433/classifier-api/v1/groups/00000000-2112-4000-8000-000000000012
-
-# Since Comply 0.8.0, scans are supposed to be run from the Comply UI only, not from the PE server.  As of 0.8.0, the following still work, but are not the officially
-# supported way to perform scans:
-# Run comply::ciscat_scan task on nodes, set comply_server to ${PROJECT}-comply.classroom.puppet.com (external address) and
-# set benchmark to "CIS_CentOS_Linux_7_Benchmark_v2.2.0-xccdf.xml" for Hydra Linux instances
+curl -s -S -k -X PUT -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" -d "{ \"name\": \"Puppet Comply Agents\", \"parent\": \"00000000-0000-4000-8000-000000000000\", \"environment\": \"${GIT_BRANCH}\", \"rule\": [\"~\", \"clientcert\", \"[win|nix]\"], \"classes\": {\"comply\": {\"linux_manage_unzip\": false} } }" https://${PROJECT}-master.classroom.puppet.com:4433/classifier-api/v1/groups/00000000-2112-4000-8000-000000000012
