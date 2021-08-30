45e53ef1c27d682e159cf4e6d9785b69d78b18f7
SE-Stephen P Potter <56322084+spotter-puppet@users.noreply.github.com>
2021-08-30 16:20:55 -0400

    Update setup.sh
    Remove allow_insecure so that we use mTLS across the board.

--- setup.sh
+++ setup.sh
@@ -16,8 +16,24 @@ fi
 ### PUPPET ENTERPRISE WORK
 
 # Need to create Puppet Comply classification and add comply node
-TOKEN=`curl -s -S -k -X POST -H 'Content-Type: application/json' -d '{"login": "admin", "password": "puppetlabs"}' https://${PROJECT}-master.classroom.puppet.com:4433/rbac-api/v1/auth/token |jq -r '.token'`
-curl -s -S -k -X POST -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" https://${PROJECT}-master.classroom.puppet.com:4433/classifier-api/v1/update-classes?environment=${ENVIR}
+TOKEN=`curl -s -S -k -X POST -H 'Content-Type: application/json' \
+        -d '{"login": "admin", "password": "puppetlabs"}' \
+        https://${PROJECT}-master.classroom.puppet.com:4433/rbac-api/v1/auth/token |jq -r '.token'`
+curl -s -S -k -X POST -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" \
+    https://${PROJECT}-master.classroom.puppet.com:4433/classifier-api/v1/update-classes?environment=${ENVIR}
 
 # Add comply to classification of any nodes you want to be scanable
-curl -s -S -k -X PUT -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" -d "{ \"name\": \"Puppet Comply Agents\", \"parent\": \"00000000-0000-4000-8000-000000000000\", \"environment\": \"${ENVIR}\", \"rule\": [\"~\", [\"fact\",\"clientcert\"], \"[win|nix]\"], \"classes\": {\"comply\": {\"linux_manage_unzip\": true, \"scanner_source\": \"https://puppet.classroom.puppet.com:8140/packages/Assessor-CLI-v4.6.0.zip\"} }, \"config_data\": {\"archive\": {\"seven_zip_provider\": \"\" }, \"comply::scanners::ciscat\": {\"allow_insecure\": true } } }" https://${PROJECT}-master.classroom.puppet.com:4433/classifier-api/v1/groups/00000000-2112-4000-8000-000000000012
+curl -s -S -k -X PUT -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" \
+    -d "{ \"name\": \"Puppet Comply Agents\", \
+    \"parent\": \"00000000-0000-4000-8000-000000000000\", \"environment\": \"${ENVIR}\", \
+    \"rule\": [\"~\", [\"fact\",\"clientcert\"], \"[win|nix]\"], \"classes\": \
+    {\"comply\": {\"linux_manage_unzip\": true, \"scanner_source\": \
+	\"https://${PROJECT}comply0.classroom.puppet.com:30303/assessor\" } }, \
+	\"config_data\": {\"archive\": {\"seven_zip_provider\": \"\" } \
+	} }" \
+    https://${PROJECT}-master.classroom.puppet.com:4433/classifier-api/v1/groups/00000000-2112-4000-8000-000000000012
+    
+curl -s -S -k -X POST -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" \
+	-d "{ \"environment\": \"\", \"enforce_environment\": false, \"scope\": { \"node_group\": \"00000000-2112-4000-8000-000000000012\" } }" \
+	https://${PROJECT}-master.classroom.puppet.com:8143/orchestrator/v1/command/deploy
+    
