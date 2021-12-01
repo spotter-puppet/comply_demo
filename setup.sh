85d0857e76f6f4411ca395ae2fd307eb86346262
SE-Stephen P Potter <56322084+spotter-puppet@users.noreply.github.com>
2021-12-01 12:01:34 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
 PROJECT=$1
-ENVIR=$2
+BACKEND=$2
+ENVIR=$3
 
 if [ "x${PROJECT}" == "x" ];
 then
-        echo "usage: $0 [project] <env>"
+        echo "usage: $0 [project] <backend> <env>"
         exit 1
+fi
+if [ "x$BACKEND" == "xdeimos" ];
+then
+                DOMAIN="se.automationdemos.com"
+                SERVER="puppet"
+else
+                DOMAIN="classroom.puppet.com"
+                SERVER="master"
 fi
 if [ "x${ENVIR}" == "x" ];
 then
@@ -18,9 +27,10 @@ fi
 # Need to create Puppet Comply classification and add comply node
 TOKEN=`curl -s -S -k -X POST -H 'Content-Type: application/json' \
         -d '{"login": "admin", "password": "puppetlabs"}' \
-        https://${PROJECT}-master.classroom.puppet.com:4433/rbac-api/v1/auth/token |jq -r '.token'`
+        https://${PROJECT}-${SERVER}.${DOMAIN}:4433/rbac-api/v1/auth/token \
+        | jq -r '.token'`
 curl -s -S -k -X POST -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" \
-    https://${PROJECT}-master.classroom.puppet.com:4433/classifier-api/v1/update-classes?environment=${ENVIR}
+    https://${PROJECT}-${SERVER}.${DOMAIN}:4433/classifier-api/v1/update-classes?environment=${ENVIR}
 
 # Add comply to classification of any nodes you want to be scanable
 curl -s -S -k -X PUT -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" \
@@ -28,12 +38,11 @@ curl -s -S -k -X PUT -H 'Content-Type: application/json' -H "X-Authentication: $
     \"parent\": \"00000000-0000-4000-8000-000000000000\", \"environment\": \"${ENVIR}\", \
     \"rule\": [\"~\", [\"fact\",\"clientcert\"], \"[win|nix]\"], \"classes\": \
     {\"comply\": {\"linux_manage_unzip\": true, \"scanner_source\": \
-	\"https://${PROJECT}comply0.classroom.puppet.com:30303/assessor\" } }, \
-	\"config_data\": {\"archive\": {\"seven_zip_provider\": \"\" } \
-	} }" \
-    https://${PROJECT}-master.classroom.puppet.com:4433/classifier-api/v1/groups/00000000-2112-4000-8000-000000000012
-    
+        \"https://${PROJECT}comply0.${DOMAIN}:30303/assessor\" } }, \
+        \"config_data\": {\"archive\": {\"seven_zip_provider\": \"\" } \
+        } }" \
+    https://${PROJECT}-${SERVER}.${DOMAIN}:4433/classifier-api/v1/groups/00000000-2112-4000-8000-000000000012
+
 curl -s -S -k -X POST -H 'Content-Type: application/json' -H "X-Authentication: $TOKEN" \
-	-d "{ \"environment\": \"\", \"enforce_environment\": false, \"scope\": { \"node_group\": \"00000000-2112-4000-8000-000000000012\" } }" \
-	https://${PROJECT}-master.classroom.puppet.com:8143/orchestrator/v1/command/deploy
-    
+        -d "{ \"environment\": \"\", \"enforce_environment\": false, \"scope\": { \"node_group\": \"00000000-2112-4000-8000-000000000012\" } }" \
+        https://${PROJECT}-${SERVER}.${DOMAIN}:8143/orchestrator/v1/command/deploy
